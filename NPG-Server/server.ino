27875146ca71103e5d1b66985706e4cc1f23cbb1
Krishnanshu Mittal <karan4g79@gmail.com>
2025-05-08 15:45:43 +0530

    Added some examples for NPG

---
+++ NPG-Server/server.ino
@@ -0,0 +1,185 @@
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
+// Copyright (c) 2024 - 2025 Upside Down Labs - contact@upsidedownlabs.tech
+// Author: Deepak Khatri
+//
+// At Upside Down Labs, we create open-source DIY neuroscience hardware and software.
+// Our mission is to make neuroscience affordable and accessible for everyone.
+// By supporting us with your purchase, you help spread innovation and open science.
+// Thank you for being part of this journey with us!
+
+/*
+ This examle is adapted from the client and server code provided by MoThunderz
+ Firmware: https://github.com/mo-thunderz/Esp32BlePart2
+ YouTube video: https://www.youtube.com/watch?v=s3yoZa6kzus
+*/
+
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+// constants won't change. They're used here to set pin numbers:
+const int buttonPin = 9;  // the number of the pushbutton pin
+const int ledPin = 7;    // the number of the LED pin
+
+// Variables will change:
+int ledState = HIGH;        // the current state of the output pin
+uint32_t buttonState;            // the current reading from the input pin
+int lastButtonState = LOW;  // the previous reading from the input pin
+
+// the following variables are unsigned longs because the time, measured in
+// milliseconds, will quickly become a bigger number than can be stored in an int.
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+// Initialize all pointers
+BLEServer* pServer = NULL;                        // Pointer to the server
+BLECharacteristic* pCharacteristic_1 = NULL;      // Pointer to Characteristic 1
+BLECharacteristic* pCharacteristic_2 = NULL;      // Pointer to Characteristic 2
+BLEDescriptor *pDescr_1;                          // Pointer to Descriptor of Characteristic 1
+BLE2902 *pBLE2902_1;                              // Pointer to BLE2902 of Characteristic 1
+BLE2902 *pBLE2902_2;                              // Pointer to BLE2902 of Characteristic 2
+
+// Some variables to keep track on device connected
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+
+// Variable that will continuously be increased and written to the client
+uint32_t value = 0;
+
+// See the following for generating UUIDs:
+// https://www.uuidgenerator.net/
+// UUIDs used in this example:
+#define SERVICE_UUID          "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define CHARACTERISTIC_UUID_1 "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define CHARACTERISTIC_UUID_2 "1c95d5e3-d8f7-413a-bf3d-7a2e5d7be87e"
+
+// Callback function that is called whenever a client is connected or disconnected
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
+    }
+};
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(buttonPin, INPUT_PULLUP);
+  pinMode(ledPin, OUTPUT);
+
+  // set initial LED state
+  digitalWrite(ledPin, ledState);
+  // Create the BLE Device
+  BLEDevice::init("ESP32");
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristic
+  pCharacteristic_1 = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_1,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );                   
+
+  pCharacteristic_2 = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID_2,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |                      
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );  
+
+  // Create a BLE Descriptor  
+  pDescr_1 = new BLEDescriptor((uint16_t)0x2901);
+  pDescr_1->setValue("A very interesting variable");
+  pCharacteristic_1->addDescriptor(pDescr_1);
+
+  // Add the BLE2902 Descriptor because we are using "PROPERTY_NOTIFY"
+  pBLE2902_1 = new BLE2902();
+  pBLE2902_1->setNotifications(true);                 
+  pCharacteristic_1->addDescriptor(pBLE2902_1);
+
+  pBLE2902_2 = new BLE2902();
+  pBLE2902_2->setNotifications(true);
+  pCharacteristic_2->addDescriptor(pBLE2902_2);
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+}
+
+void loop() {
+  // read the state of the switch into a local variable:
+  int reading = digitalRead(buttonPin);
+
+  // check to see if you just pressed the button
+  // (i.e. the input went from LOW to HIGH), and you've waited long enough
+  // since the last press to ignore any noise:
+
+  // If the switch changed, due to noise or pressing:
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer than the debounce
+    // delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == HIGH) {
+        ledState = !ledState;
+        if(deviceConnected){
+                pCharacteristic_1->setValue(ledState);
+      pCharacteristic_1->notify();
+        }
+      }
+    }
+  }
+
+  // set the LED:
+  digitalWrite(ledPin, ledState);
+
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        Serial.println("start advertising");
+        oldDeviceConnected = deviceConnected;
+    }
+    // Connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+  // save the reading. Next time through the loop, it'll be the lastButtonState:
+  lastButtonState = reading;
+}
