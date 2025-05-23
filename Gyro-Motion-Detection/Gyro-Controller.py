8f643f1760316722cc4d3a7b2ab184ab4d67589f
Krishnanshu Mittal <karan4g79@gmail.com>
2025-05-23 05:36:54 +0530

    Added gyro-controller

--- Gyro-Motion-Detection/Gyro-Controller.py
+++ Gyro-Motion-Detection/Gyro-Controller.py
@@ -1,3 +1,24 @@
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+#  Copyright (c) 2025 Krishnanshu Mittal - krishnanshu@upsidedownlabs.tech
+#  Copyright (c) 2025 Upside Down Labs - contact@upsidedownlabs.tech
+
+#  At Upside Down Labs, we create open-source DIY neuroscience hardware and software.
+#  Our mission is to make neuroscience affordable and accessible for everyone.
+#  By supporting us with your purchase, you help spread innovation and open science.
+#  Thank you for being part of this journey with us!
+
 import serial
 import threading
 import time
